73fa74a5542bfa1a32fa1043a6b5f7157c15dfd0
Srdjan Grubor <sgnn7@sgnn7.org>
2013-12-06 10:58:18 -0600

    Made history be a bit better with multiple terminals

--- bin/bashrc_extras.sh
+++ bin/bashrc_extras.sh
@@ -2,6 +2,15 @@ export PATH=~/bin:$PATH
 
 . fix_prompt.sh
 
+export HISTSIZE=100000
+export HISTFILESIZE=100000
+
+# append to history
+shopt -s histappend
+
+# Save and reload the history after each command finishes
+export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
+
 alias cvm='ssh dev1@vm1'
 alias cvm2='ssh dev1@vm2'
 alias sus='sudo -u sg -- gnome-screensaver-command -l && sudo pm-suspend'
