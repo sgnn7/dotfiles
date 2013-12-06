7ffc9830031712adb500978400a81225acddc343
Srdjan Grubor <sgnn7@sgnn7.org>
2013-12-06 10:59:24 -0600

    Made sus be a bit more generic

--- bin/bashrc_extras.sh
+++ bin/bashrc_extras.sh
@@ -13,7 +13,7 @@ export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
 
 alias cvm='ssh dev1@vm1'
 alias cvm2='ssh dev1@vm2'
-alias sus='sudo -u sg -- gnome-screensaver-command -l && sudo pm-suspend'
+alias sus='sudo -u $USER -- gnome-screensaver-command -l && sudo pm-suspend'
 alias ddst='ps -ef | grep dd | grep sudo | awk "{print \$2}" | xargs sudo kill -USR1'
 
 stty -ixon
