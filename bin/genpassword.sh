6bcf5086b7462ea3e3f88186795f5efc13a38ade
Srdjan Grubor <sgnn7@sgnn7.org>
2014-02-19 17:09:47 -0600

    Added option to change default password length

--- bin/genpassword.sh
+++ bin/genpassword.sh
@@ -2,8 +2,13 @@
 
 genpasswd() {
     local l=$1
-    [ "$l" == "" ] && l=16
+    [ "$l" == "" ] && l=$length
     tr -dc A-Za-z0-9_ < /dev/urandom | head -c ${l} | xargs
 }
 
-genpasswd
+length=16
+if [ $# -ne 0 ]; then
+  length=$1
+fi
+
+genpasswd $1
