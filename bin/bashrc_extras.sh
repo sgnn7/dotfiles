0975ed41cf9d05f2e4602b9758e472be7ca98e5e
Srdjan Grubor <sgnn7@sgnn7.org>
2014-04-18 10:07:48 -0500

    Made ssh use a bit more secure set of algos

--- bin/bashrc_extras.sh
+++ bin/bashrc_extras.sh
@@ -11,8 +11,11 @@ shopt -s histappend
 # Save and reload the history after each command finishes
 export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
 
+alias ssh='ssh -c aes256-cbc -m hmac-sha1'
+
 alias cvm='ssh dev1@vm1'
 alias cvm2='ssh dev1@vm2'
+
 alias sus='sudo -u $USER -- gnome-screensaver-command -l && sudo pm-suspend'
 alias ddst='ps -ef | grep dd | grep sudo | awk "{print \$2}" | xargs sudo kill -USR1'
 
