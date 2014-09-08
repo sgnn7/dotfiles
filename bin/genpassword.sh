9842e0f9cec5ef45ca1997dfd4b255c9423bbbc6
Srdjan Grubor <sgnn7@sgnn7.org>
2014-09-08 10:29:13 -0500

    Increased keyspace for genpassword

--- bin/genpassword.sh
+++ bin/genpassword.sh
@@ -3,7 +3,7 @@
 genpasswd() {
     local l=$1
     [ "$l" == "" ] && l=$length
-    tr -dc A-Za-z0-9_ < /dev/urandom | head -c ${l} | xargs
+    tr -dc 'A-Za-z0-9_x\@\#\!\-\+\=\(\)\~\[\]\{\}.,;:' < /dev/urandom | head -c ${l} | xargs -0
 }
 
 length=16
