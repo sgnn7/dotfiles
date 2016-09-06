8bf4fabcff0173c5d6d70a352f35015b299fa3f8
Srdjan Grubor <sgnn7@sgnn7.org>
2016-09-06 15:19:07 -0500

    Added UUID randomization for raw_to_vdi script
    UUID is reused on some images so we want to be able to use more of these
    within the same VBox installation.

--- bin/raw_to_vdi.sh
+++ bin/raw_to_vdi.sh
@@ -14,25 +14,28 @@ if [ $# -gt 1 ]; then
   VDI_SIZE="$2"
 fi
 
-IMAGE_NAME="${COMPRESSED_IMAGE%.*}"
+IMAGE_NAME="${COMPRESSED_IMAGE%.img.gz}"
 
 echo "Image name: ${IMAGE_NAME}"
 echo "Using VDI size of ${VDI_SIZE} GB"
 
 echo "- Decompressing $IMAGE_NAME..."
 if [ ! -f "${IMAGE_NAME}.img" ]; then
-  gunzip -k $COMPRESSED_IMAGE
+  gunzip -k "$COMPRESSED_IMAGE"
 fi
 
 echo "- Converting $IMAGE_NAME to VDI..."
 # Assume that the compressed image is a '*.img'
 if [ ! -f "${IMAGE_NAME}.vdi" ]; then
-  vboxmanage convertdd $IMAGE_NAME.img $IMAGE_NAME.vdi --format VDI
+  vboxmanage convertdd "$IMAGE_NAME.img" "$IMAGE_NAME.vdi" --format VDI
 fi
 
 rm -f "${IMAGE_NAME}.img"
 
 echo "- Expanding $IMAGE_NAME to $VDI_SIZE GB..."
-vboxmanage modifyhd $IMAGE_NAME.vdi --resize "$(( $VDI_SIZE * 1024 ))"
+vboxmanage modifyhd "$IMAGE_NAME.vdi" --resize "$(( $VDI_SIZE * 1024 ))"
+
+echo "- Randomizing UUID..."
+vboxmanage internalcommands sethduuid "${IMAGE_NAME}.vdi"
 
 echo "OK!"
