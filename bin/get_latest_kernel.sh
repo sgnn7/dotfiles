44a354383c0c609eda475da1a6c78dce0b6b75a8
Srdjan Grubor <sgnn7@sgnn7.org>
2017-07-15 05:25:35 -0500

    Improved get_latest_kernel UX and we get source files too
    Improved the UX quite a bit:
     - Better progress
     - Better formatting
     - No silent bail on checksum fail
    
    Also added fetching of sources/commit/patch info for recreatable
    kernels.

--- bin/get_latest_kernel.sh
+++ bin/get_latest_kernel.sh
@@ -5,10 +5,12 @@ BASE_URI="http://kernel.ubuntu.com/~kernel-ppa/mainline"
 FINGERPRINT="60AA 7B6F 3043 4AE6 8E56  9963 E50C 6A09 17C6 22B0"
 ARCH=$(dpkg --print-architecture)
 
-declare -A PACKAGES=([linux-image]="linux-image- generic_ ${ARCH}" \
-                     [linux-headers]="linux-headers- generic_ ${ARCH}" \
-                     [linux-headers-all]="linux-headers- _all\.deb" \
-                    )
+declare -A FILES=([sources]="SOURCES" \
+                  [commit]="COMMIT" \
+                  [linux-image]="linux-image- generic_ ${ARCH}" \
+                  [linux-headers]="linux-headers- generic_ ${ARCH}" \
+                  [linux-headers-all]="linux-headers- _all\.deb" \
+                 )
 
 echo "Grabbing the GPG key"
 gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys "${FINGERPRINT}"
@@ -30,6 +32,17 @@ echo "Version: ${highest_version}"
 #  echo "Version ${highest_version} already installed. Skipping"
 #  exit 1
 #fi
+
+verify_checksum() {
+  filename="${1}"
+  sha_sum=$(sha256sum $filename | awk '{print $1}')
+  if ! grep "^${sha_sum}.*${filename}$" CHECKSUMS >/dev/null; then
+    echo "Failed to verify ${filename}!"
+    exit 1
+  fi
+
+  echo "${filename}: Checksum OK!"
+}
 
 echo "Getting the version page..."
 version_page=$(curl -sL "${version_uri}")
@@ -55,19 +68,33 @@ pushd "${target_dir}" >/dev/null
     exit 1
   fi
 
-  for filters in "${PACKAGES[@]}"; do
+  for filters in "${FILES[@]}"; do
     # echo "Filters: $filters"
     package_line="${version_page}"
     for filter in ${filters}; do
       package_line=$(echo "${package_line}" | grep "${filter}")
     done
-    package_name=$(echo "${package_line}" | sed -n '1p' | sed -e 's/.*<a href="linux-\([^"]*\).*/linux-\1/')
-    echo "Downloading ${package_name}"
-    wget "${version_uri}/${package_name}" -O "${package_name}"
 
-    echo "Verifying checksum for ${package_name}"
-    sha_sum=$(sha256sum $package_name | awk '{print $1}')
-    grep "^${sha_sum}.*${package_name}$" CHECKSUMS >/dev/null
+    package_name=$(echo "${package_line}" | sed -n '1p' | sed -e 's/.*<a href="\([^"]*\).*/\1/')
+    # echo "Downloading ${package_name}"
+    wget -q --show-progress "${version_uri}/${package_name}" -O "${package_name}"
+
+    # SOURCES isn't included in checksums
+    if [ "${package_name}" == "SOURCES" ]; then
+      continue
+    fi
+
+    verify_checksum "${package_name}"
   done
-  echo "All good!"
+
+  echo "Downloading patches..."
+  for patch in $(cat SOURCES | grep "^.*\.patch$"); do
+    # echo "Downloading ${patch}"
+    wget -q --show-progress "${version_uri}/${patch}" -O "${patch}"
+
+    verify_checksum "${patch}"
+  done
+
+  echo
+  echo "=== SUCCESS ==="
 popd >/dev/null
